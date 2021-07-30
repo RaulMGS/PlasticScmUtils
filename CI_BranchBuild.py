659384bbe80c3f31c991e419f7b44b65a885cb2a
MadGoatStudio <turcusraul2712@gmail.com>
2021-07-30 12:31:42 +0300

    update discord announce, add unity build ci

---
+++ CI_BranchBuild.py
@@ -0,0 +1,88 @@
+import os
+import re
+import sys
+from discord_webhook import DiscordWebhook
+from discord_webhook import DiscordEmbed
+
+# Script setup
+WEBHOOK_URL = 'WEBHOOK_URL'
+
+# Embed content formats
+EMBED_TITLE_FORMAT = 'New Northern Lights build started'
+EMBED_ICON_URL = 'https://i.imgur.com/koyPU5S.png'
+EMBED_DESCRIPTION_FORMAT = '{user} started a new Unity Build'
+
+# Build Utils
+BUILD_TARGET_BRANCH = 'main/staging'
+BUILD_UNITY_PATH = 'D:/Work/Unity Installs/2019.4.26f1/Editor'
+BUILD_WORKSPACE_PATH = 'E:/Work/Unity Projects/NorthernLights-Release'
+BUILD_RESULT_PATH = 'E:/Work/Unity Builds/NorthernLights-Release'
+
+#-Implementation---------------------------------
+# Data parsing
+def parse_plastic_stdin():
+    change_branch = '', change_count = 0
+    for line in sys.stdin:
+        change_count = change_count + 1
+        if not change_branch:
+            change_branch = re.search(r"\#br:(.+?)\;", line).group()
+            change_branch = change_branch.replace("#br:/", "").replace(";", "")
+
+    return change_branch, change_count
+
+def parse_plastic_user(): 
+    return os.environ['PLASTIC_USER']
+
+# Discord API wrappers
+def get_embed_for(title, contents, change_count, change_branch):
+    if not title:
+        title = 'MISSING_TITLE'
+    if not contents:
+        contents = 'MISSING_CONTENTS'
+    if not change_branch:
+        change_branch = 'MISSING_BRANCH'
+
+    embed_instance = DiscordEmbed(
+        title=title,
+        description=contents)
+    embed_instance.add_embed_field(name='Files Changed', value=change_count)
+    embed_instance.add_embed_field(name='Branch', value=change_branch)
+    embed_instance.set_timestamp()
+    embed_instance.set_color('03b2f8')
+    embed_instance.set_thumbnail(url=EMBED_ICON_URL)
+    return embed_instance
+
+# Runtime Utils
+def run_webhook(url, embed): 
+    webhook_instance = DiscordWebhook(url=url)
+    webhook_instance.add_embed(embed) 
+    webhook_instance.execute()
+
+def run_build_pipeline():
+    os.chdir(BUILD_WORKSPACE_PATH)
+    os.system("cm update")
+    os.chdir(BUILD_UNITY_PATH)
+    os.system("Unity.exe -quit -batchmode -projectpath \"{inpath}\" -buildWindows64Player \"{outpath}\"".format(
+        inpath = BUILD_WORKSPACE_PATH,
+        outpath = BUILD_RESULT_PATH
+    ))
+
+#-Runtime-----------------------------------------
+branch, changes = parse_plastic_stdin()
+user = parse_plastic_user()
+
+# We only want to continue if we are on the build branch. We don't want to start builds from checkins 
+# that are on branches irrelevant to the building procedure
+if branch == BUILD_TARGET_BRANCH:
+    # Compose embed for webhook message
+    embed = get_embed_for(
+        EMBED_TITLE_FORMAT, 
+        EMBED_DESCRIPTION_FORMAT.format(user=user), 
+        changes, 
+        branch)
+
+    # Send webhook post to discord announcing that a build has started
+    run_webhook(WEBHOOK_URL, embed)
+
+    # Run unity build pipeline
+    run_build_pipeline()
--- CI_DiscordAnnounce.py
+++ CI_DiscordAnnounce.py
@@ -1,65 +1,103 @@
 import os
+import sys
 import datetime
+import re
 from discord_webhook import DiscordWebhook
 from discord_webhook import DiscordEmbed
 
-# Script Setup
+# Script setup
 WEBHOOK_URL = 'WEBHOOK_URL'
 WEBHOOK_KEEP_FILES = False
 
-# Checkin formats
-EMBED_TITLE_FORMAT = 'New commit in {branch}'
-EMBED_ICON_URL = ''
-EMBED_DESCRIPTION_FORMAT = '**{user} checked in a new changeset** \n\n**Comments:**\n{comment}'
-EMBED_DESCRIPTIONLONG_FORMAT = '**{user} checked in a new changeset** \n\n**For comments check file (more than 200 characters):**'
+# Embed content formats
+EMBED_TITLE_FORMAT = 'New check-in in {branch}'
+EMBED_ICON_URL = 'https://i.imgur.com/koyPU5S.png'
+EMBED_DESCRIPTION_FORMAT = '{user} checked in a new changeset \n\n**Comments:**\n{comment}'
+EMBED_DESCRIPTIONLONG_FORMAT = '{user} checked in a new changeset \n\n**For comments check file (more than 200 characters)**'
 
-# get change count
-change_count = 0
-for line in sys.stdin:
-    change_count = change_count + 1
+#-Implementation---------------------------------
+# Data parsing
+def parse_plastic_stdin():
+    change_branch = '', change_count = 0
+    for line in sys.stdin:
+        change_count = change_count + 1
+        if not change_branch:
+            change_branch = re.search(r"\#br:(.+?)\;", line).group()
+            change_branch = change_branch.replace("#br:/", "").replace(";", "")
+            
+    return change_branch, change_count    
 
-# TODO: Fetch branch somehow
-change_branch = 'main'
+def parse_plastic_envvars():
+    embed_contents = EMBED_DESCRIPTION_FORMAT.format(user=os.environ['PLASTIC_USER'], comment=os.environ['PLASTIC_COMMENT'])
+    if len(os.environ['PLASTIC_COMMENT']) > 1500:
+        embed_contents = EMBED_DESCRIPTIONLONG_FORMAT.format(user=os.environ['PLASTIC_USER'])
+    return embed_contents
 
-# get formatted time
-now = datetime.datetime.now()
-timestr = now.strftime("%Y_%m_%d_%H_%M_%S")
+def parse_datetime():
+    return datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
 
-# get webhook string message
-embed_contents = EMBED_DESCRIPTION_FORMAT.format(
-    user=os.environ['PLASTIC_USER'], comment=os.environ['PLASTIC_COMMENT'])
-if len(os.environ['PLASTIC_COMMENT']) > 1500:
-    embed_contents = EMBED_DESCRIPTIONLONG_FORMAT.format(
-        user=os.environ['PLASTIC_USER'])
-
-# construct embed
-embed_instance = DiscordEmbed(
-    title=EMBED_TITLE_FORMAT.format(branch=change_branch),
-    description=embed_contents)
-embed_instance.add_embed_field(name='Files Changed', value=change_count)
-embed_instance.add_embed_field(name='Branch', value=change_branch)
-embed_instance.set_timestamp()
-embed_instance.set_color('03b2f8')
-
-webhook_instance = DiscordWebhook(url=WEBHOOK_URL)
-webhook_instance.add_embed(embed_instance)
-
-# add file to webhook if char limit reached
-if len(os.environ['PLASTIC_COMMENT']) > 1500:
-
-    # write to file.
-    path = "{}_{}.txt".format(timestr, os.environ['PLASTIC_USER'])
+# File logging
+def mk_logfile(path): 
     file = open(path, "w")
-    file.write(EMBED_DESCRIPTION_FORMAT.format(os.environ['PLASTIC_USER'], change_count, os.environ['PLASTIC_COMMENT']))
+    file.write(EMBED_DESCRIPTION_FORMAT.format(
+        os.environ['PLASTIC_USER'],  
+        os.environ['PLASTIC_COMMENT']))
     file.close()
 
-    # Load file to webhook
-    with open(path, "rb") as f:
-        webhook_instance.add_file(file=f.read(), filename=path)
-
+def rm_logfile(path):
     # remove file when done
     if os.path.exists(path) and (not WEBHOOK_KEEP_FILES):
         os.remove(path)
 
-# post webhook
-webhook_instance.execute()
+# Discord API wrappers
+def get_embed_for(title, contents, change_count, change_branch):
+    if not title:
+        title = 'MISSING_TITLE'
+    if not contents:
+        contents = 'MISSING_CONTENTS'
+    if not change_branch:
+        change_branch = 'MISSING_BRANCH'
+
+    embed_instance = DiscordEmbed(
+        title=title,
+        description=contents)
+    embed_instance.add_embed_field(name='Files Changed', value=change_count)
+    embed_instance.add_embed_field(name='Branch', value=change_branch)
+    embed_instance.set_timestamp()
+    embed_instance.set_color('03b2f8')
+    embed_instance.set_thumbnail(url = EMBED_ICON_URL)
+    return embed_instance
+
+def post_webhook(url, embed):
+    # construct webhook message
+    webhook_instance = DiscordWebhook(url=url)
+    webhook_instance.add_embed(embed)
+
+    # add file to webhook if char limit reached
+    if len(os.environ['PLASTIC_COMMENT']) > 1500:
+        # Get path for file
+        path = "{}_{}.txt".format(parse_datetime(), os.environ['PLASTIC_USER'])
+
+        # Create log file from checkins
+        mk_logfile(path)
+
+        # Load file to webhook
+        with open(path, "rb") as f:
+            webhook_instance.add_file(file=f.read(), filename=path)
+
+        # Remove log file
+        rm_logfile(path)
+
+    # post webhook
+    webhook_instance.execute()
+
+#-Runtime-----------------------------------------
+# get vars for embed
+branch, changes = parse_plastic_stdin()
+title = EMBED_TITLE_FORMAT.format(branch=branch)
+comments = parse_plastic_envvars()
+timestamp = parse_datetime()
+
+# get embed and execute webhook
+embed = get_embed_for(title, comments, changes, branch)
+post_webhook(WEBHOOK_URL, embed)
